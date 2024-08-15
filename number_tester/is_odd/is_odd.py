0ef3ff3214a61868d0a88c0737fd744322795805
Phil Vendola <pv72895@gmail.com>
2024-08-15 15:58:26 -0400

    Change is_odd

--- number_tester/is_odd/is_odd.py
+++ number_tester/is_odd/is_odd.py
@@ -1,7 +1,7 @@
 from number_tester.is_number.is_number import is_number
-
+from number_tester.is_even.is_even import is_even
 
 def is_odd(input_string):
     if is_number(input_string):
-        return int(input_string) % 2 != 0
+        return not is_even(input_string)
     return False
