ddeb01bc9d04392d0399696f026bfcee8b188e08
Phil Vendola <pv72895@gmail.com>
2024-08-15 15:53:41 -0400

    Change is_even function

--- number_tester/is_even/is_even.py
+++ number_tester/is_even/is_even.py
@@ -2,6 +2,6 @@ from number_tester.is_number.is_number import is_number
 
 
 def is_even(input_string):
-    if is_number(input_string):
-        return int(input_string) % 2 == 0
+    if is_number(input_string) and int(input_string) % 2 == 0:
+        return True
     return False
